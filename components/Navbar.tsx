4e478a9a8ec437bc64241de98fecf1e22f6adc41
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-10-11 20:26:31 +0200

    Add responsivity navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,47 @@
 import KolpingLogo from "./KolpingLogo";
+import clsx from "clsx";
+import { useState } from "react";
 
 export const Navbar = () => {
+	const [showNavigation, setShowNavigation] = useState(true);
+
 	return (
-		<nav className="h-16 flex justify-between items-center text-white z-20 relative">
-			<div>
-				<KolpingLogo />
-			</div>
-			<div>
-				<ul className="flex space-x-6 font-medium">
+		<>
+			<nav className="h-16 flex justify-between items-center text-white z-40 relative">
+				<div>
+					<KolpingLogo />
+				</div>
+				<ul className="hidden md:flex space-x-6 font-medium">
+					<li>O nas</li>
+					<li>Teatr</li>
+					<li>Działania</li>
+					<li>
+						<a
+							href="/kontakt"
+							className="bg-white p-3 pr-5 pl-5 rounded-md text-secondary-blue"
+						>
+							Kontakt
+						</a>
+					</li>
+				</ul>
+				<div
+					className="md:hidden bg-white p-3 pr-5 pl-5 rounded-md text-secondary-blue cursor-pointer select-none"
+					onClick={() => {
+						setShowNavigation((s) => !s);
+					}}
+				>
+					Menu
+				</div>
+			</nav>
+			<div
+				className={clsx(
+					"md:hidden absolute w-full h-screen left-0 top-0 bg-secondary-blue z-30",
+					{
+						hidden: showNavigation,
+					}
+				)}
+			>
+				<ul className="w-full h-full flex justify-center items-center flex-col space-y-12">
 					<li>O nas</li>
 					<li>Teatr</li>
 					<li>Działania</li>
@@ -21,6 +55,6 @@ export const Navbar = () => {
 					</li>
 				</ul>
 			</div>
-		</nav>
+		</>
 	);
 };
