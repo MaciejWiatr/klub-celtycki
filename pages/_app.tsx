847b0c1aee6a6bfbf16b51ea6b8159870395235a
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-10-11 23:55:22 +0200

    Ensure that navbar will always be closed after route transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,22 @@ import "tailwindcss/tailwind.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
+import { useRouter } from "next/router";
+import useNavbar from "../store/useNavbar";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+	const { setShowNavbar } = useNavbar();
+
+	useEffect(() => {
+		router.events.on("routeChangeComplete", handleRouteChange);
+	}, []);
+
+	const handleRouteChange = () => {
+		setShowNavbar(false);
+	};
+
 	return (
 		<main className="font-mul">
 			<Component {...pageProps} />
--- store/useNavbar.ts
+++ store/useNavbar.ts
@@ -3,10 +3,14 @@ import create from "zustand";
 interface NavbarState {
 	showNavbar: Boolean;
 	toggleNavbar: () => void;
+	setShowNavbar: (val: boolean) => void;
 }
 
 const useNavbar = create<NavbarState>((set) => ({
 	showNavbar: false,
+	setShowNavbar: (val: boolean) => {
+		set(() => ({ showNavbar: val }));
+	},
 	toggleNavbar: () => {
 		set(({ showNavbar }) => ({ showNavbar: !showNavbar }));
 		document.body.classList.toggle("overflow-hidden");
