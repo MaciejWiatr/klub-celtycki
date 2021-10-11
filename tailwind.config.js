7c6816c05ac7872157b4755369d74fd18de3cd1a
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-10-11 20:04:27 +0200

    Add tailwind and typescript config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	mode: "jit",
+	purge: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				mul: "Mulish, sans-serif",
+				mont: "Montserrat, sans-serif",
+			},
+			colors: {
+				"primary-blue": "#4FC1DF",
+				"primary-orange": "#F58220",
+				"secondary-blue": "#243059",
+				text: "#3A3A3C",
+			},
+			height: {
+				fullpage: "100vh",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
