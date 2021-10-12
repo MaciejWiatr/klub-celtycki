2b4f131fc10d1a47d6351884ab67a92f227dacbb
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-10-12 22:40:21 +0200

    Start working on kontakt page

---
+++ pages/kontakt.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "../components/Navbar";
+
+const KontaktPage = () => {
+	return (
+		<>
+			<div className="flex justify-center">
+				<div className="max-w-6xl w-full text-black">
+					<Navbar />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default KontaktPage;
