faf6ddbbdc72a3d88c560d49672fb782b56f1e0f
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-10-13 10:02:52 +0200

    Fix navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,27 @@
 import KolpingLogo from "./KolpingLogo";
 import useNavbar from "../store/useNavbar";
 import { NavLink } from "./NavLink";
+import { FC } from "react";
+import clsx from "clsx";
 
-export const Navbar = () => {
+interface IProps {
+	reversed?: boolean;
+}
+
+export const Navbar: FC<IProps> = ({ reversed = false }) => {
 	const { showNavbar, toggleNavbar } = useNavbar();
 
 	return (
 		<>
 			<nav className="h-16 flex justify-between items-center text-white z-50 relative">
 				<a href="/">
-					<KolpingLogo />
+					<KolpingLogo reversed={reversed && !showNavbar} />
 				</a>
-				<ul className="hidden md:flex space-x-6 font-medium">
+				<ul
+					className={clsx("hidden md:flex space-x-6 font-medium", {
+						"text-black": reversed,
+					})}
+				>
 					<li>
 						<NavLink href="/#onas">O nas</NavLink>
 					</li>
@@ -19,26 +29,38 @@ export const Navbar = () => {
 						<NavLink href="/teatr">Teatr</NavLink>
 					</li>
 					<li>
-						<NavLink href="/dzialania">Działania</NavLink>
+						<NavLink href="/dzialania">Nasze działania</NavLink>
 					</li>
 					<li className="hover:text-primary-orange">
 						<a
 							href="/kontakt"
-							className="bg-white p-3 pr-5 pl-5 rounded-md text-secondary-blue"
+							className={clsx("p-3 pr-5 pl-5 rounded-md", {
+								"bg-secondary-blue text-white": reversed,
+								"bg-white p-3 pr-5 pl-5 rounded-md text-secondary-blue":
+									!reversed,
+							})}
 						>
 							Kontakt
 						</a>
 					</li>
 				</ul>
 				<div
-					className="md:hidden bg-white p-3 pr-5 pl-5 rounded-md text-secondary-blue cursor-pointer select-none"
+					className={clsx(
+						"md:hidden  p-3 pr-5 pl-5 rounded-md  cursor-pointer select-none",
+						{
+							"bg-white text-secondary-blue":
+								!reversed || showNavbar,
+							"bg-secondary-blue text-white":
+								reversed && !showNavbar,
+						}
+					)}
 					onClick={toggleNavbar}
 				>
 					{showNavbar ? "Zamknij menu" : "Menu"}
 				</div>
 			</nav>
 			{showNavbar && (
-				<div className="z-40 md:hidden fixed w-full h-screen left-0 top-0 bg-secondary-blue">
+				<div className="z-40 md:hidden fixed w-full h-screen left-0 top-0 bg-secondary-blue text-white">
 					<ul className="w-full h-full flex justify-center items-center flex-col space-y-12">
 						<li>
 							<NavLink href="/#onas">O nas</NavLink>
