1e62f33378f82ea3130e9dd2e95e7498b4bb856d
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-10-13 22:36:31 +0200

    Fix navlink structure

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export const NavLink = ({ href, children }) => {
 	return (
-		<Link href={href}>
-			<span className="hover:text-primary-orange transition cursor-pointer">
-				{children}
-			</span>
+		<Link href={href} passHref>
+			<a>
+				<span className="hover:text-primary-orange transition cursor-pointer">
+					{children}
+				</span>
+			</a>
 		</Link>
 	);
 };
